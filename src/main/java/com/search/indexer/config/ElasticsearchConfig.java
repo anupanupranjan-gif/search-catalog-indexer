b46f37b50799a3ecdc1d2fb7ed3cbce60204d905
Anup Ranjan <223038265+anupanupranjan-gif@users.noreply.github.com>
2026-03-06 21:36:58 -0600

    fix: add SSL trust-all config for ECK self-signed cert

--- src/main/java/com/search/indexer/config/ElasticsearchConfig.java
+++ src/main/java/com/search/indexer/config/ElasticsearchConfig.java
@@ -9,7 +9,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.nio.conn.ssl.SSLIOSessionStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.elasticsearch.client.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,28 +20,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.net.ssl.SSLContext;
+
 @Configuration
 public class ElasticsearchConfig {
-
     private static final Logger log = LoggerFactory.getLogger(ElasticsearchConfig.class);
 
     @Value("${elasticsearch.host:localhost}")
     private String host;
-
     @Value("${elasticsearch.port:9200}")
     private int port;
-
     @Value("${elasticsearch.username:elastic}")
     private String username;
-
     @Value("${elasticsearch.password:changeme}")
     private String password;
-
     @Value("${elasticsearch.scheme:http}")
     private String scheme;
 
     @Bean
-    public ElasticsearchClient elasticsearchClient() {
+    public ElasticsearchClient elasticsearchClient() throws Exception {
         log.info("Connecting to Elasticsearch at {}://{}:{}", scheme, host, port);
 
         var credentialsProvider = new BasicCredentialsProvider();
@@ -47,16 +47,29 @@ public class ElasticsearchConfig {
                 new UsernamePasswordCredentials(username, password)
         );
 
-        var restClient = RestClient.builder(new HttpHost(host, port, scheme))
-                .setHttpClientConfigCallback(httpClientBuilder ->
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+        var restClientBuilder = RestClient.builder(new HttpHost(host, port, scheme))
                 .setRequestConfigCallback(requestConfigBuilder ->
                         requestConfigBuilder
                                 .setConnectTimeout(5000)
-                                .setSocketTimeout(60000))
-                .build();
+                                .setSocketTimeout(60000));
 
-        // Register JavaTimeModule so Instant serializes correctly
+        if ("https".equalsIgnoreCase(scheme)) {
+            SSLContext sslContext = SSLContextBuilder.create()
+                    .loadTrustMaterial(null, (chains, authType) -> true)
+                    .build();
+            SSLIOSessionStrategy sslStrategy = new SSLIOSessionStrategy(
+                    sslContext, NoopHostnameVerifier.INSTANCE);
+            restClientBuilder.setHttpClientConfigCallback(httpClientBuilder ->
+                    httpClientBuilder
+                            .setDefaultCredentialsProvider(credentialsProvider)
+                            .setSSLStrategy(sslStrategy));
+        } else {
+            restClientBuilder.setHttpClientConfigCallback(httpClientBuilder ->
+                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+        }
+
+        var restClient = restClientBuilder.build();
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
